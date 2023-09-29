6c1cd91bcab8e5d843b00d0c85395bf7c10ac0ed
tomaszpro99 <tomaszprostko99@gmail.com>
2023-09-29 03:17:52 +0200

    mchatpro app with icon

---
+++ app/src/main/java/io/github/tomaszpro99/mchatproapp/MainActivity.java
@@ -0,0 +1,38 @@
+package io.github.tomaszpro99.mchatproapp;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import androidx.appcompat.app.AppCompatActivity;
+public class MainActivity extends AppCompatActivity {
+
+    private WebView webView;
+
+    @SuppressLint("SetJavaScriptEnabled")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        webView = findViewById(R.id.webView);
+        webView.setWebViewClient(new WebViewClient());
+
+        // Włącz obsługę JavaScript
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+
+        // Wyświetl stronę mchat.pro
+        webView.loadUrl("http://mchat.pro/");
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
